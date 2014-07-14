edb17c5c8a9541e46cfefbb20621a19ecc4d71e5
Paul Roberts <psxpaul@gmail.com>
2014-07-14 12:16:22 -0700

    Extracting a lot of logic into a separate OfficeServer object

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,78 +1,23 @@
-require(["jquery", "Badge"], function($, Badge) {
-    var office365Url = "https://outlook.office365.com/",
-        feedUrl = office365Url + "ews/odata/Me/Inbox/Messages?$filter=IsRead%20eq%20false",
-        needsAuthentication = false,
-        badge = new Badge();
-
-    function isOffice365Url(url) {
-        return url.indexOf(office365Url) === 0 || url.indexOf(".outlook.com/owa") !== -1;
-    }
-
+require(["jquery", "OfficeServer", "Badge"], function($, OfficeServer, Badge) {
     function updateInboxCount() {
-        if(needsAuthentication) {
-            console.log("skipping updating unread count, as authentication is still needed");
-            return;
-        }
+        Badge.startLoadingAnimation();
+        console.log("updating unread count...");
 
-        console.log("updating unread count");
-        badge.startLoadingAnimation();
-        $.ajax({
-            url: feedUrl,
-            statusCode: {
-                401: function() {
-                    needsAuthentication = true;
-                }
-            },
-            success: function(data) {
-                needsAuthentication = false;
-                badge.stopLoadingAnimation();
-
-                var unreadCount = data.value.length;
-                badge.setUnreadCount(unreadCount);
+        OfficeServer.getUnreadCount({
+            success: function(unreadCount) {
+                Badge.stopLoadingAnimation();
+                Badge.setUnreadCount(unreadCount);
             },
             error: function() {
-                badge.stopLoadingAnimation();
-                badge.setUnreadCount();
+                Badge.stopLoadingAnimation();
                 console.log("error: ");
                 console.dir(arguments);
             }
         });
     }
 
-    chrome.webNavigation.onDOMContentLoaded.addListener(function() {
-        needsAuthentication = false;
-    }, { url: [{urlEquals: feedUrl}] });
-
-    chrome.webNavigation.onDOMContentLoaded.addListener(updateInboxCount, { url: [{urlContains: office365Url}] });
-    chrome.webNavigation.onReferenceFragmentUpdated.addListener(updateInboxCount, { url: [{urlContains: office365Url}] });
-
-    chrome.browserAction.onClicked.addListener(function() {
-        if(needsAuthentication) {
-            chrome.tabs.create({url: feedUrl}, function(tab) {
-                chrome.tabs.executeScript(tab.id, {code: "window.close();"});
-            });
-        }
-
-        chrome.tabs.getAllInWindow(undefined, function(tabs) {
-            var foundTab = false;
-
-            $.each(tabs, function(i, tab) {
-                if (tab.url && isOffice365Url(tab.url)) {
-                    foundTab = foundTab || (tab.url !== feedUrl);
-
-                    if(!needsAuthentication) {
-                        chrome.tabs.update(tab.id, {active: true});
-                        foundTab = true;
-                    }
-                    updateInboxCount();
-                }
-            });
-
-            if(!foundTab) {
-                chrome.tabs.create({url: office365Url, active: !needsAuthentication});
-            }
-        });
-    });
+    chrome.webNavigation.onDOMContentLoaded.addListener(updateInboxCount, OfficeServer.chromeUrlFilter);
+    chrome.webNavigation.onReferenceFragmentUpdated.addListener(updateInboxCount, OfficeServer.chromeUrlFilter);
 
     chrome.alarms.create('office365-checker', { periodInMinutes: 1 });
     chrome.alarms.onAlarm.addListener(function(alarm) {
---
+++ src/js/OfficeServer.js
@@ -0,0 +1,67 @@
+define(["jquery"], function($) {
+    var office365Url = "https://outlook.office365.com/",
+        feedUrl = office365Url + "ews/odata/Me/Inbox/Messages?$filter=IsRead%20eq%20false",
+        needsAuthentication = false;
+
+    function isOffice365Url(url) {
+        return url.indexOf(office365Url) === 0 || url.indexOf(".outlook.com/owa") !== -1;
+    }
+
+    function getUnreadCount(opts) {
+        if(needsAuthentication) {
+            console.log("skipping updating unread count, as authentication is still needed");
+            return;
+        }
+
+        $.ajax({
+            url: feedUrl,
+            statusCode: {
+                401: function() {
+                    needsAuthentication = true;
+                }
+            },
+            success: function(data) {
+                needsAuthentication = false;
+                opts.success(data.value.length);
+            },
+            error: opts.error
+        });
+    }
+
+    chrome.webNavigation.onDOMContentLoaded.addListener(function() {
+        needsAuthentication = false;
+    }, { url: [{urlEquals: feedUrl}] });
+
+    chrome.browserAction.onClicked.addListener(function() {
+        if(needsAuthentication) {
+            chrome.tabs.create({url: feedUrl}, function(tab) {
+                chrome.tabs.executeScript(tab.id, {code: "window.close();"});
+            });
+        }
+
+        chrome.tabs.getAllInWindow(undefined, function(tabs) {
+            var foundTab = false;
+
+            $.each(tabs, function(i, tab) {
+                if (tab.url && isOffice365Url(tab.url)) {
+                    foundTab = foundTab || (tab.url !== feedUrl);
+
+                    if(!needsAuthentication) {
+                        chrome.tabs.update(tab.id, {active: true});
+                        foundTab = true;
+                    }
+                    updateInboxCount();
+                }
+            });
+
+            if(!foundTab) {
+                chrome.tabs.create({url: office365Url, active: !needsAuthentication});
+            }
+        });
+    });
+
+    return {
+        getUnreadCount: getUnreadCount,
+        chromeUrlFilter: { url: [{urlContains: office365Url}] }
+    };
+});
